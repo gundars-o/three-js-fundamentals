a9d664563a5f7991c17abe481545e75db2b155f7
gundars-o <gundars.ozolins75@gmail.com>
2021-01-27 08:39:57 +0200

    3 rotating cubes rendered.

--- fundamentals.js
+++ fundamentals.js
@@ -16,8 +16,6 @@ const boxWidth = 1;
 const boxHeight = 1;
 const boxDepth = 1;
 const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
-/********** MATERIAL **********/
-const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } );
 /********** ADD LIGHT **********/
 const color = 0xFFFFFF;
 const intensity = 1;
@@ -25,15 +23,28 @@ const light = new THREE.DirectionalLight( color, intensity );
 light.position.set( -1, 2, 4 );
 light.target.position.set( 0, 0, 0 );
 scene.add( light );
-/********** CREATE ELEMENT **********/
-const cube = new THREE.Mesh( geometry, material );
-/********** ADD TO SCENE **********/
-scene.add( cube );
+/********** CREATE ELEMENTS **********/
+function makeInstance( geometry, color, x ) {
+    const material = new THREE.MeshPhongMaterial( { color } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    cube.position.x = x;
+    return cube;
+};
+const cubes = [
+    makeInstance( geometry, 0x44aa88,  0 )
+    , makeInstance( geometry, 0x8844aa, -2 )
+    , makeInstance( geometry, 0xaa8844,  2 )
+];
 /********** RENDER SCENE **********/
 function render( time /* the time since the page loaded to function */ ) {
     time *= 0.001 /* convert time to seconds */;
-    cube.rotation.x /* radians */ = time;
-    cube.rotation.y = time;
+    cubes.forEach( ( cube, ndx ) => {
+        const speed = 1 + ndx * 0.1;
+        const rot = time * speed;
+        cube.rotation.x /* radians */ = rot;
+        cube.rotation.y = rot;
+    } );
     renderer.render( scene, camera );
     requestAnimationFrame( render );
 };
