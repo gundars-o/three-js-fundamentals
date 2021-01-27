3d9709b7f0bdff88fff0e07c5cb93b822f403fe8
gundars-o <gundars.ozolins75@gmail.com>
2021-01-27 15:43:59 +0200

    GridHelper and dat.GUI added.

--- sun.js
+++ sun.js
@@ -1,7 +1,9 @@
-import * as THREE from './build/three.module.js';
+import * as THREE from "./build/three.module.js";
+import {GUI} from "./examples/jsm/libs/dat.gui.module.js";
 /********** CREATE RENDERER **********/
-const canvas = document.querySelector( '#c' );
+const canvas = document.querySelector( "#c" );
 const renderer = new THREE.WebGLRenderer( {canvas} );
+const gui = new GUI();
 /********** CREATE CAMERA **********/
 const fov = 40;
 const aspect = 2 /* the canvas defaul */;
@@ -59,12 +61,44 @@ moonMesh.scale.set( 0.5, 0.5, 0.5 );
 moonOrbit.add( moonMesh );
 objects.push( moonMesh );
 /********** AxesHelper to each node **********/
-objects.forEach( ( node ) => {
-    const axes = new THREE.AxesHelper();
-    axes.material.depthTest = false;
-    axes.renderOrder = 1;
-    node.add( axes );
-} );
+    // Turns both axes and grid visible on/off
+    // dat.GUI requires a property that returns a bool
+    // to decide to make a checkbox so we make a setter
+    // and getter for `visible` which we can tell dat.GUI
+    // to look at.
+class AxisGridHelper {
+    constructor( node, units = 10 ) {
+        const axes = new THREE.AxesHelper();
+        axes.material.depthTest = false;
+        axes.renderOrder = 2 /* after the grid */;
+        node.add( axes );
+        const grid = new THREE.GridHelper( units, units );
+        grid.material.depthTest = false;
+        grid.renderOrder = 1;
+        node.add( grid );
+        this.grid = grid;
+        this.axes = axes;
+        this.visible = false;
+    };
+    get visible() {
+        return this._visible;
+    };
+    set visible( v ) {
+        this._visible = v;
+        this.grid.visible = v;
+        this.axes.visible = v;
+    };
+};
+function makeAxisGrid( node, label, units ) {
+    const helper = new AxisGridHelper( node, units );
+    gui.add( helper, "visible" ).name( label );
+};
+makeAxisGrid( solarSystem, "solarSystem", 25 );
+makeAxisGrid( sunMesh, "sunMesh" );
+makeAxisGrid( earthOrbit, "earthOrbit" );
+makeAxisGrid( earthMesh, "earthMesh" );
+makeAxisGrid( moonOrbit, "moonOrbit" );
+makeAxisGrid( moonMesh, "moonMesh" );
 /********** RENDERER SIZE **********/
 function resizeRendererToDisplaySize( renderer ) {
     const canvas = renderer.domElement;
